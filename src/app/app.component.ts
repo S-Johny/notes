8216f5737d9899c1280ea6b3398bcd34c0dfc8f4
Jan Strapek <honstrap@gmail.com>
2020-04-19 20:41:42 +0200

    feat(store): added translations + some showcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { LanguageService, Language } from './language.service';
+import { pluck } from 'rxjs/operators';
+import { Store, select } from '@ngrx/store';
+import { AppState } from './store/store.model';
+import {
+  getAllNotesAction,
+  getNoteAction,
+  createNoteAction,
+} from './store/note.actions';
+import { $tableNotes, $note } from './store/note.selectors';
+import { Observable } from 'rxjs';
+import { NoteModel } from '../api/api.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'notes-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'notes-task';
+  readonly selectedLanguage$ = this.languageService
+    .languageChanged()
+    .pipe(pluck('lang'));
+  readonly notesTableData$ = this.store.pipe(select($tableNotes));
+
+  noteForm: FormGroup = this.fb.group({
+    title: ['', Validators.required],
+    description: '',
+  });
+  constructor(
+    private readonly store: Store<AppState>,
+    private readonly languageService: LanguageService,
+    private readonly fb: FormBuilder,
+  ) {
+    this.store.dispatch(getAllNotesAction());
+  }
+
+  changeLanguage(lang: Language): void {
+    this.languageService.setLanguage(lang);
+  }
+
+  getDetail(id: number): void {
+    this.store.dispatch(getNoteAction({ id }));
+  }
+
+  noteDetail(id: number): Observable<NoteModel | null> {
+    return this.store.pipe(select($note(id)));
+  }
+
+  addNote(): void {
+    if (this.noteForm.invalid) {
+      return;
+    }
+    const note: NoteModel = {
+      id: 3,
+      title: this.noteForm.get('title')?.value,
+      description: this.noteForm.get('description')?.value,
+    };
+    this.store.dispatch(createNoteAction({ note }));
+    this.noteForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,62 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducerMap } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApiService } from '../api/api.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { LanguageService } from './language.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { AppState } from './store/store.model';
+import { noteReducer } from './store/note.reducers';
+import { NoteEffects } from './store/note.effects';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, 'assets/translation/', '.json');
+}
+
+const reducers: ActionReducerMap<AppState> = {
+  notes: noteReducer,
+};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    MatExpansionModule,
+    MatToolbarModule,
+    MatButtonToggleModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
     }),
-    EffectsModule.forRoot([]),
+    EffectsModule.forRoot([NoteEffects]),
     BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [ApiService, LanguageService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
